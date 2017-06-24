89c97b422c4833dd8b362407f619e6c1aa37e697
Julius Seporaitis <seporaitis@users.noreply.github.com>
2017-06-24 23:01:47 +0100

    Add more request factory tests (#13)

--- polls/tests/test_views_with_request_factory.py
+++ polls/tests/test_views_with_request_factory.py
@@ -1,9 +1,10 @@
+from django.http import Http404
 from django.utils import timezone
 from django.urls import reverse
 
 from polls.models import Choice, Question
 from polls.tests import base
-from polls.views import IndexView, vote
+from polls.views import DetailView, IndexView, ResultsView, vote
 
 
 class QuestionViewTests(base.BaseTestCase):
@@ -64,6 +65,57 @@ class QuestionViewTests(base.BaseTestCase):
 
         self.assertContains(response, question1.question_text)
         self.assertContains(response, question2.question_text)
+
+
+class QuestionIndexDetailsTests(base.BaseTestCase):
+
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = self.create_question(question_text='Future question.', days=5)
+        request = self.request_factory.get(reverse('polls:detail', args=(future_question.id,)))
+        with self.assertRaises(Http404):
+            DetailView.as_view()(request, pk=future_question.id)
+
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = self.create_question(question_text='Past Question.', days=-5)
+        request = self.request_factory.get(reverse('polls:detail', args=(past_question.id,)))
+        response = DetailView.as_view()(request, pk=past_question.id)
+        self.assertContains(response, past_question.question_text)
+
+
+class QuestionResultsTests(base.BaseTestCase):
+
+    def test_results_empty(self):
+        """
+        The results view of a question should display choices and counts.
+        """
+        question = self.create_question(question_text='Question.', days=-5)
+        choice1 = Choice.objects.create(
+            question=question,
+            choice_text='Choice 1',
+            votes=10,
+        )
+        choice2 = Choice.objects.create(
+            question=question,
+            choice_text='Choice 2',
+            votes=11,
+        )
+
+        request = self.request_factory.get(reverse('polls:results', args=(question.id,)))
+        response = ResultsView.as_view()(request, pk=question.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<li>{} -- {} votes</li>".format(choice1.choice_text, choice1.votes))
+        self.assertContains(response, "<li>{} -- {} votes</li>".format(choice2.choice_text, choice2.votes))
+
 
 
 class VoteTests(base.BaseTestCase):
