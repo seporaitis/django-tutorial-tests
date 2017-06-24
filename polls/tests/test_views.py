52e719397d8e2a50c4e543b159abbfef74a13489
Julius Seporaitis <seporaitis@users.noreply.github.com>
2017-06-24 16:00:07 +0100

    Split out tests.py into separate files. (#7)

--- polls/tests/test_views.py
+++ polls/tests/test_views.py
@@ -5,38 +5,8 @@ from django.utils import timezone
 from django.urls import reverse
 from django.test import TestCase, RequestFactory
 
-from .models import Choice, Question
-from .views import ResultsView, vote
-
-
-class QuestionMethodTests(TestCase):
-
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() should return False for questions whose
-        pub_date is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently() should return False for questions whose
-        pub_date is older than 1 day.
-        """
-        time = timezone.now() - datetime.timedelta(days=30)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() should return True for questions whose
-        pub_date is within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=1)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+from polls.models import Choice, Question
+from polls.views import ResultsView, vote
 
 
 def create_question(question_text, days):
