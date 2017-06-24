2eba96c78663562fb837e34631e0d62e64c990dc
Julius Seporaitis <seporaitis@users.noreply.github.com>
2017-06-24 18:30:39 +0100

    Tests for IndexView using RequestFactory (#10)

--- polls/tests/test_views_with_request_factory.py
+++ polls/tests/test_views_with_request_factory.py
@@ -1,17 +1,93 @@
+import datetime
+
 from django.utils import timezone
 from django.urls import reverse
 from django.test import TestCase, RequestFactory
 
 from polls.models import Choice, Question
-from polls.views import vote
+from polls.views import IndexView, vote
 
 
-class VoteTests(TestCase):
+class BaseTestCase(TestCase):
 
     def setUp(self):
         super().setUp()
 
         self.request_factory = RequestFactory()
+
+    def create_question(self, question_text, days):
+        """
+        Creates a question with the given `question_text` and published the
+        given number of `days` offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTests(BaseTestCase):
+
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        request = self.request_factory.get(reverse('polls:index'))
+        response = IndexView.as_view()(request)
+
+        self.assertContains(response, "No polls are available")
+
+    def test_index_view_with_a_past_question(self):
+        """
+        Questions with a pub_date in the past should be displayed on the
+        index page.
+        """
+        question = self.create_question(question_text="Past question", days=-30)
+        request = self.request_factory.get(reverse('polls:index'))
+        response = IndexView.as_view()(request)
+
+        self.assertContains(response, question.question_text)
+
+    def test_index_view_with_a_future_question(self):
+        """
+        Questions with a pub_date in the future should not be displayed on
+        the index page.
+        """
+        question = self.create_question(question_text="Future question", days=30)
+        request = self.request_factory.get(reverse('polls:index'))
+        response = IndexView.as_view()(request)
+
+        self.assertNotContains(response, question.question_text)
+
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        should be displayed.
+        """
+        past_question = self.create_question(question_text="Past question", days=-30)
+        future_question = self.create_question(question_text="Future question", days=30)
+        request = self.request_factory.get(reverse('polls:index'))
+        response = IndexView.as_view()(request)
+
+        self.assertContains(response, past_question.question_text)
+        self.assertNotContains(response, future_question.question_text)
+
+    def test_index_view_with_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        question1 = self.create_question(question_text="Past question", days=-30)
+        question2 = self.create_question(question_text="Past question", days=-5)
+        request = self.request_factory.get(reverse('polls:index'))
+        response = IndexView.as_view()(request)
+
+        self.assertContains(response, question1.question_text)
+        self.assertContains(response, question2.question_text)
+
+
+class VoteTests(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
 
         self.question = Question.objects.create(
             question_text='Is this a test?',
@@ -33,7 +109,7 @@ class VoteTests(TestCase):
         )
         self.choice2.save()
 
-    def test_vote(self):
+    def test_view(self):
         url = reverse('polls:vote', args=(self.question.id,))
         request = self.request_factory.post(url, {'choice': self.choice2.id})
         response = vote(request, self.question.id)
